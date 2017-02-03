b0bbbe1dde99af917dc7755010a8d5e329337ce4
durov_an <durov_an@magnit.ru>
2017-02-03 16:06:35 +0300

    продолжаем рефакторинг.. + эксперименты с автотестами

---
+++ src/test/groovy/com/tander/logistics/DbReleasePluginTest.groovy
@@ -0,0 +1,41 @@
+package com.tander.logistics
+
+import org.gradle.api.Project
+import org.gradle.internal.impldep.org.junit.Test
+import org.gradle.testfixtures.ProjectBuilder
+
+/**
+ * Created by durov_an on 02.02.2017.
+ */
+class DbReleasePluginTest extends GroovyTestCase {
+    Project project
+    DbReleasePlugin plugin
+    void setUp() {
+        super.setUp()
+        project = ProjectBuilder.builder().build()
+        plugin = new DbReleasePlugin()
+        plugin.apply(project)
+    }
+
+    void tearDown() {
+
+    }
+
+    void testApply() {
+
+        DbReleaseExtension ext = project.extensions.findByType(DbReleaseExtension)
+        "execution of buildDbRelease task is success"()
+
+
+
+    }
+    @Test
+    def "execution of buildDbRelease task is success"() {
+        when:
+        project
+
+        then:
+        project.getTasksByName('buildDbRelease', true).size() == 1
+
+    }
+}
